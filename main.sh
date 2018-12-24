090e695aa7eda7f3ab28818459a323b99c40814e
RyoTakata <takata@hal.ipc.i.u-tokyo.ac.jp>
2018-12-24 17:18:47 +0900

    test tgt finished

--- main.sh
+++ main.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 set -Ceu
 
-SCRIPT_PATH=$(cd $(dirname $0) && pwd)
-SCRIPT_NAME=$(basename $0)
-
-
-echo ${SCRIPT_PATH}
-echo ${SCRIPT_NAME}
-echo $$
-
-python hoge.py &
-TGT_PID=$!
-sleep 5 &
-TIM_PID=$!
-
-echo "TGT_PID: "${TGT_PID}
-echo "TIM_PID: "${TIM_PID}
-
-
-
-wait
-echo "eof"
+./timeout.sh &
+./timeout.sh &
+./timeout.sh &
+./timeout.sh &
+./timeout.sh &
+./timeout.sh &
--- timeout.sh
+++ timeout.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -Ceu
 
 SCRIPT_PATH=$(cd $(dirname $0) && pwd)
 SCRIPT_NAME=$(basename $0)
@@ -9,5 +8,29 @@ echo ${SCRIPT_PATH}
 echo ${SCRIPT_NAME}
 echo $$
 
-timeout 10 python hoge.py 
-# timeout 10 ./hoge.sh
+### [1] timeout
+# timeout 5 ./hoge.sh
+### [2] sleep
+./hoge.sh &
+TGT_PID=$!
+sleep 5 &
+TIM_PID=$!
+
+echo "TGT_PID: "${TGT_PID}
+echo "TIM_PID: "${TIM_PID}
+
+while true; do
+    kill -0 ${TGT_PID}
+    if [ $? -eq 0 ]; then
+        kill ${TIM_PID}
+        exit
+    fi
+    kill -0 ${TIM_PID}
+    if [ $? -eq 0 ]; then
+        exit
+    fi
+    sleep 1
+done
+
+wait
+
